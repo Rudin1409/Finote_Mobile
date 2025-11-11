14878d831fe553b8d87521db9b55154b965535f3
mbahrudin140906 <mbahrudin140906@gmail.com>
2025-11-11 14:22:07 +0000

    splash awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:myapp/screens/auth/welcome_screen.dart';
-import 'package:myapp/screens/main_screen.dart';
+import 'package:myapp/screens/onboarding/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // Set the initial route to the welcome screen
-      initialRoute: '/welcome',
-      // Define the routes for your application
-      routes: {
-        '/welcome': (context) => const WelcomeScreen(),
-        '/main': (context) => const MainScreen(),
-      },
-      // You can remove the 'home' property when using 'initialRoute'
-      // home: const MainScreen(), 
+      home: const OnboardingScreen(),
     );
   }
 }
